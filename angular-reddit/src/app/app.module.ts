ee63908336d9a3d70d0519f0559fbf78dc7e7446
EC2 Default User <ec2-user@ip-172-31-30-27.ec2.internal>
2019-02-27 13:52:16 +0000

    redditlikeappiamalexago

---
+++ angular-reddit/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { ArticleComponent } from './article/article.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArticleComponent
+  ],
+  imports: [
+    BrowserModule, NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
